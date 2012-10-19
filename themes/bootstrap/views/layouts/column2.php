b97cbea0234aa0a3e7d22333addae2b76800f589
Hisateru Tanaka <tanakahisateru@gmail.com>
2012-10-19 23:10:32 +0900

    メニューがカラム落ちするような小さい画面の場合はドロップダウンボタンでメニューを出す

--- themes/bootstrap/views/layouts/column2.php
+++ themes/bootstrap/views/layouts/column2.php
@@ -1,12 +1,23 @@
 <?php /* @var $this Controller */ ?>
 <?php $this->beginContent('//layouts/main'); ?>
+
+<div class="visible-phone">
+	<?php
+		$this->widget('bootstrap.widgets.TbButtonGroup', array(
+			'buttons'=>array(
+				array('label'=>'Operations', 'icon'=>'cog', 'items'=>$this->menu),
+			)
+		));
+	?>
+</div>
+
 <div class="row">
 <div class="span9">
 	<div id="content">
 		<?php echo $content; ?>
 	</div><!-- content -->
 </div>
-<div class="span3">
+<div class="span3 hidden-phone">
 	<div id="sidebar">
 	<?php
 		$this->beginWidget('zii.widgets.CPortlet', array(
